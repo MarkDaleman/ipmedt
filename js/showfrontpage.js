7dd24c32f8a0eccb5a3032ac9c8dc5c897a2c4a0
Mark Daleman <markdaleman@gmail.com>
2015-09-30 14:34:07 +0200

    Tijdlijn 2 werkt nu zeker wolla

---
+++ js/showfrontpage.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Mark on 30/09/15.
+ */
+$(document).ready(function(){
+   $("#openingsfilm").show();
+    $("#fullpage").hide();
+    $(".nav").hide();
+
+
+    $(document).click(function(){
+        $("#openingsfilm").hide();
+        $("#fullpage").show();
+        $(".nav").show();
+
+    });
+});
+
